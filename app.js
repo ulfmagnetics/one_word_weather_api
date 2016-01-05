2740f5dac0db556702a8a186d68d45c110c1f4eb
John Berry <ulfmagnetics@gmail.com>
2016-01-04 17:37:05 -0800

    Add business logic to pull most recent tweet from @onewordsfwthr

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/weather', function(req, res) {
-  res.send('hello weather');
+app.locals.DEFAULT_LOCALITY = 'sf';
+app.locals.screenNames = {
+  sf: 'onewordsfwthr'
+};
+
+var defaultLocality = function(req, res, next) {
+  if (!req.params.locality) {
+    req.params.locality = app.locals.DEFAULT_LOCALITY;
+  }
+  next();
+}
+
+app.get('/word/:locality?', defaultLocality, function(req, res) {
+  if (app.locals.screenNames[req.params.locality] !== undefined) {
+    req.screenName = app.locals.screenNames[req.params.locality];
+    var options = { screenName: req.screenName };
+    var timeline = require('./timeline')(options);
+    var error = function(err) { res.sendStatus(503); }
+    var success = function(result) {
+      res.type('text/plain').send(result);
+    }
+    timeline.mostRecent(error, success)
+  }
+  else {
+    res.sendStatus(404);
+  }
 });
 
 var server = app.listen(3000, function() {
---
+++ timeline.js
@@ -0,0 +1,32 @@
+var Twitter = require('twitter-node-client').Twitter;
+
+require('./env.js');
+var config = {
+  "consumerKey":        process.env['CONSUMER_KEY'],
+  "consumerSecret":     process.env['CONSUMER_SECRET'],
+  "accessToken":        process.env['ACCESS_TOKEN_KEY'],
+  "accessTokenSecret":  process.env['ACCESS_TOKEN_SECRET']
+};
+var twitter = new Twitter(config);
+
+var timeline = function(options) {
+  return {
+    mostRecent: function(error, success) {
+      var error = function(data) {
+
+      }
+      var twitterOpts = {screen_name: options.screenName, count: '1'};
+      twitter.getUserTimeline(twitterOpts,
+        function(err)  {
+          error(err);
+        },
+        function(json) {
+          var tweets = JSON.parse(json);
+          success(tweets[0].text);
+        }
+      );
+    }
+  }
+};
+
+module.exports = timeline;
